8bfc954aee828015ebf4ffbf4b0e4baf9e91add2
Nrj9272 <neerajjha4411@gmail.com>
2021-01-28 15:51:22 +0530

    Modify variable value and pom.xml

--- src/main/java/com/ashokit/Application.java
+++ src/main/java/com/ashokit/Application.java
@@ -8,10 +8,10 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-		int a=10;
-		int b=20;
-		int c=a+b;
-		System.out.println(c);
+		int a=100;
+		int b=200;
+		int p=a+b;
+		System.out.println(p);
 	}
 
 }
